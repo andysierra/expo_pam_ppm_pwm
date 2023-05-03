cb5ad76b00018c7490bd96f815cb2301f04c0a02
andysierra <camilo-hormiga@hotmail.com>
2023-05-03 18:59:52 -0500

    last chance

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: "/expo_pam_ppm_pwm/", // to work with github pages
+  base: "/expo_pam_ppm_pwm/", // to work with github pages
   plugins: [react()]
 })
